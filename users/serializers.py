e1b0ab73aced128fa0b518dd38c072a6ff657cd9
joesphchang <joesphschang@gmail.com>
2022-02-11 20:55:01 -0500

    Scaffoled out user serializers and models

---
+++ users/serializers.py
@@ -0,0 +1,10 @@
+from djoser.serializers import UserCreateSerializer
+from rest_framework import serializers
+from . import models
+
+
+class UserCreateSerializer(UserCreateSerializer):
+
+    class Meta(UserCreateSerializer.Meta):
+        model = models.User
+        fields = ('id', 'email', 'username', 'password', 'avatar')
