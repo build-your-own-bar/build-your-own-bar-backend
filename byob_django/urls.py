85112bacf8f6317648a4a8c438fd13e5f86822f7
thejustinlombardi <thejustinlombardi@gmail.com>
2022-02-12 11:05:34 -0500

    Deploy test 7

--- byob_django/urls.py
+++ byob_django/urls.py
@@ -19,8 +19,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth', include('rest_framework.urls', namespace = 'rest_framework')),
+    # path('admin/', admin.site.urls),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('users.urls')),
     path('', include('byob.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
